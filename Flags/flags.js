4ec756dc2024bf82a1956099ebe98c5b62414404
LuigiBelanda <luigi.belanda.milani@gmail.com>
2021-09-06 09:24:45 -0300

    Entendo um pouco sobre flags

---
+++ Flags/flags.js
@@ -0,0 +1,17 @@
+// Input: node Flags/flags.js --name "Luigi Belanda" --greeting "Ola, mundo"
+
+console.log(process.argv);
+
+/*
+Output:
+
+[
+  '/usr/local/bin/node',
+  '/home/luigi/Programing/NodeJS-Rocketseat/Flags/flags.js',
+  '--name',
+  'Luigi Belanda',
+  '--greeting',
+  'Ola, mundo'
+]
+
+*/
